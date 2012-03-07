397a30b76d846783f1fed5628ed9ada81eaa372d
Tadas Gricina <gricina.tadas@gmail.com>
2012-03-07 12:29:18 +0200

    komentaras

--- src/os2/GUI.java
+++ src/os2/GUI.java
@@ -1,5 +1,23 @@
 package os2;
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import javax.swing.JLabel;
+
+public class GUI extends MouseAdapter implements ActionListener {
     
+    private OS2 os2;
+    private JLabel registersL, ramL;
+    
+    public GUI(final OS2 os2){
+        this.os2 = os2;
+        
+    }
+        
+    public void actionPerformed(ActionEvent e) {
+        
+    };
+    
+     
 }
--- src/os2/VirtualMachine.java
+++ src/os2/VirtualMachine.java
@@ -7,7 +7,12 @@ public class VirtualMachine {
     public ArrayList<Register> virtualRegisters = new ArrayList<Register>();
     
     public VirtualMachine(){
-        
+        virtualRegisters.add(new Register("IR", 2, new int[]{0, 0}));
+        virtualRegisters.add(new Register("SF", 1, new int[]{0}));
+        virtualRegisters.add(new Register("DF", 1, new int[]{0}));
+        virtualRegisters.add(new Register("IP", 2, new int[]{0, 1}));
+        virtualRegisters.add(new Register("R", 4, new int[]{0, 0, 0, 0}));
+        virtualRegisters.add(new Register("SP", 2, new int[]{0, 0}));
     }
     
 }
