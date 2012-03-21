dd89c76223052dfef161b1947c8c3505a19f33f9
Tadas Gricina <gricina.tadas@gmail.com>
2012-03-21 16:12:41 +0200

    GUI

--- src/os2/RAM.java
+++ src/os2/RAM.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class RAM {
     
     int supervisorMemorySize = 256;
-    int userMemorySize = 255;
+    int userMemorySize = 256;
     int wordSize = 4;
     int[][] words;
     
     public RAM(){
         int allMemorySize = supervisorMemorySize + userMemorySize;
         words = new int[allMemorySize][wordSize];
-        for (int i = 0; i<=allMemorySize-1; i++){
+        for (int i = 0; i<allMemorySize; i++){
                 words[i] = new int[]{0, 0, 0, 0};
         }
     }
@@ -23,5 +23,12 @@ public class RAM {
     
     public int[] getWord(int x, int y) {
        return words[x * 16 + y];
+    }
+    
+    public String getStringValueAt(int x, int y){
+        String s = null;
+        s = Integer.toString(words[x * 16 + y][0]) + Integer.toString(words[x * 16 + y][1]) +
+            Integer.toString(words[x * 16 + y][2]) + Integer.toString(words[x * 16 + y][3]);
+        return s;
     }
 }
